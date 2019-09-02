436102cd32ccf9e1ce837c45512ed52ddd4eb7d7
Max <Max@101-10>
2019-09-02 18:11:47 +0300

    splitted_on_file_done

---
+++ ForwardList/ForwardList.h
@@ -0,0 +1,8 @@
+#pragma once
+#include<iostream>
+
+#define tab "\t"
+#define delimiter "\n----------------------------------------------------------------------------------\n"
+using namespace std;
+
+template<typename T>class ForwardList;
